c1e92278443a61f3b0f4b23e471446cb010d5a0c
Andzuo <alucasm02@gmail.com>
2024-05-30 22:04:05 -0300

    refact: refactoring the form build

--- hooks/use-actions.ts
+++ hooks/use-actions.ts
@@ -33,10 +33,7 @@ export const useAction = <Tinput, TOutput>(
             if (!result) {
                return;
             }
-
-            if (result.fieldErrors) {
-               setFieldErrors(result.fieldErrors);
-            }
+            setFieldErrors(result.fieldErrors);
 
             if (result.error) {
                setError(result.error);
